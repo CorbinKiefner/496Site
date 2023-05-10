30e317636ab10f7bfda258753d89e62ff866b87d
Corbin Kiefner <corbinkiefner319@gmail.com>
2023-05-10 07:15:01 -0400

    Revert "Recipes Pt.2"
    This reverts commit 76dfb27a9f5fcf674b925a3ac40414dec9bc0c57.

--- src/pages/recipe.js
+++ src/pages/recipe.js
@@ -2,21 +2,12 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
-import { graphql } from 'gatsby'
 
 // Step 2: Define your component
-const RecipesPage = props => {
-
-  const data = {
-    nodeFood: props.pageContext.data
-  }
-
-  console.log(data);
-
+const RecipesPage = () => {
   return (
     <Layout pageTitle="Recipes">
       <p>Recipe page.</p>
-      <h2>{DataTransfer.nodeFood?.title}</h2>
     </Layout>
   )
 }
