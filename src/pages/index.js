1d040b3a78fd0aea8e03e35067328bdfc36f0c3b
Corbin Kiefner <corbinkiefner319@gmail.com>
2023-05-10 06:18:36 -0400

    Test - Updated recipe.js
    Commented out all of the code under recipe.js to see if that is what is causing the gatsby cloud build to fail. Also made the file path to for the image on the homepage relative.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="FISH!!!!!!"
-        src="C:\Users\corbi\Desktop\496site\src\images\fishpic.jpg"
+        src="src\images\fishpic.jpg"
       />
     </Layout>
   )
